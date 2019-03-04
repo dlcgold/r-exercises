d09c3c4b1662955c154b5688bb774ce8b9bb3d70
fdila <federicadilauro1998@gmail.com>
2019-03-04 10:10:36 +0100

    aggiunti esercizi primo lab

--- es1/es.R
+++ es1/es.R
@@ -1,2 +1,16 @@
-plot(table(years))
-plot(prop.table(table(years)))
+Giocatori <- read.csv("~/Documents/UniShare/Statistica/r-exercises/es1/Giocatori.txt", sep=";", comment.char="#")
+attach(Giocatori)
+#frequenza assoluta
+table(years)
+#frequenza relativa
+prop.table(table(years))
+#frequenze cumulate relative
+cumsum(prop.table(table(years)))
+#frequenze cumulate assolute
+cumsum(table(years))
+#min
+min(years)
+#max
+max(years)
+#istogramma
+hist(years,main="Istogramma età")
